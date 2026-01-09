8288453539437736044f1d0300037c4e9e49cded
Efe Yusuf Karademir <efeyusuf.karademir@bahcesehir.edu.tr>
2026-01-09 14:43:25 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -100,6 +100,7 @@ void bSearchTreeType<T>::insert(const T& item) {
     // TODO:
     // Call the recursive insert helper starting from root.
     // Example: insert(root, item);
+    insert(root,item);
 }
 
 /************************************************************
@@ -111,6 +112,13 @@ void bSearchTreeType<T>::insert(const T& item) {
 template <class T>
 void bSearchTreeType<T>::insert(nodeType<T>*& p, const T& item) {
     // TODO:
+    if(p==nullptr){
+    p=new nodeType<T>(item);
+    return;
+    }
+    if(item<p->info){ insert(p->llink,item);
+    }else{insert(p->rlink,item);
+    }
     // If p is null, create a new node.
     // Otherwise, recurse left or right based on BST rules.
 }
@@ -121,16 +129,19 @@ void bSearchTreeType<T>::insert(nodeType<T>*& p, const T& item) {
 template <class T>
 void bSearchTreeType<T>::inorderTraversal() const {
     // TODO: call inorder(root)
+    inorder(root);
 }
 
 template <class T>
 void bSearchTreeType<T>::preorderTraversal() const {
     // TODO: call preorder(root)
+    preorder(root);
 }
 
 template <class T>
 void bSearchTreeType<T>::postorderTraversal() const {
     // TODO: call postorder(root)
+    postorder(root);
 }
 
 /************************************************************
@@ -143,19 +154,35 @@ template <class T>
 void bSearchTreeType<T>::inorder(nodeType<T>* p) const {
     // TODO:
     // If p is null return
+    if(p==nullptr) return;
     // Visit left, print root, visit right
+    inorder(p->llink);
+    cout << p->info << " ";
+    inorder(p->rlink);
 }
 
 template <class T>
 void bSearchTreeType<T>::preorder(nodeType<T>* p) const {
     // TODO:
     // If p is null return
+    if(p==nullptr) return;
+    else{
+        cout << p->info << " ";
+        preorder(p->llink);
+        preorder(p->rlink);
+    }
     // Print root, visit left, visit right
 }
 
 template <class T>
 void bSearchTreeType<T>::postorder(nodeType<T>* p) const {
     // TODO:
+    if(p==nullptr) return;
+    else{
+        postorder(p->llink);
+        postorder(p->rlink);
+        cout << p->info << " ";
+    }
     // If p is null return
     // Visit left, visit right, print root
 }
@@ -169,15 +196,19 @@ void bSearchTreeType<T>::postorder(nodeType<T>* p) const {
 template <class T>
 int bSearchTreeType<T>::treeHeight() const {
     // TODO: return height(root)
-    return 0;
+    return height(root);
 }
 
 template <class T>
 int bSearchTreeType<T>::height(nodeType<T>* p) const {
     // TODO:
+    if(p==nullptr) return 0;
+    else{
+        return 1+((height(p->llink)<height(p->rlink)) ? height(p->rlink) : height(p->llink));
+      // return 1 + max(height(p->llink),height(p->rlink));
+    }
     // If p is null => 0
     // else => 1 + max(height(left), height(right))
-    return 0;
 }
 
 /************************************************************
@@ -186,15 +217,18 @@ int bSearchTreeType<T>::height(nodeType<T>* p) const {
 template <class T>
 int bSearchTreeType<T>::treeNodeCount() const {
     // TODO: return nodeCount(root)
-    return 0;
+    return nodeCount(root);
 }
 
 template <class T>
 int bSearchTreeType<T>::nodeCount(nodeType<T>* p) const {
     // TODO:
+    if(p==nullptr) return 0;
+    else{
+        return 1 + nodeCount(p->llink) + nodeCount(p->rlink);
+    }
     // If p is null => 0
     // else => 1 + nodeCount(left) + nodeCount(right)
-    return 0;
 }
 
 /************************************************************
@@ -204,13 +238,18 @@ int bSearchTreeType<T>::nodeCount(nodeType<T>* p) const {
 template <class T>
 int bSearchTreeType<T>::treeLeavesCount() const {
     // TODO: return leavesCount(root)
-    return 0;
+    return leavesCount(root);
 }
 
 template <class T>
 int bSearchTreeType<T>::leavesCount(nodeType<T>* p) const {
     // TODO:
-    // If p is null => 0
+    if(p==nullptr) return 0;
+    if(p->rlink == nullptr && p->llink == nullptr) return 1;
+     else
+     return leavesCount(p->llink) + leavesCount(p->rlink);
+  
+    // If p  is null => 0
     // If p is leaf => 1
     // else => leavesCount(left) + leavesCount(right)
     return 0;
